499919ebaea6738b4668c2a98cae04b11bbd41b0
Shasa Thuo <shasa.thuo@gmail.com>
2024-10-21 21:42:04 +0300

    Handling Images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 
 export default function AboutPage() {
   return (
-    <div>I am the about page</div>
+    <div>Wallapangalaz my bru</div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,26 @@
+import styles from "./page.module.css"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-       Homepage
+    <div className={styles.container}>
+      <div className={styles.textcontainer}>
+        <h1 className={styles.title}>We do Creative tings</h1>
+        <div className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imagecontainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
     </div>
   );
 }
